d886ea43ba41c438985a58c46fc7e5b2828fc5a6
kimsehw <seunghwan9703@gmail.com>
2024-11-25 20:54:26 +0900

    fix(OutputView): 출력 양식 및 오류 수정 및 해결

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,6 +12,7 @@ public class OutputView {
         System.out.println("<할인 전 총주문 금액>");
         if (receipt.isCanEvent()) {
             System.out.println(String.format("%,d원", receipt.getTotalAmount()));
+            System.out.println();
             System.out.println("<증정 메뉴>");
             if (receipt.isCanGetEvent()) {
                 System.out.println("샴페인 1개");
@@ -19,6 +20,7 @@ public class OutputView {
             if (!receipt.isCanGetEvent()) {
                 System.out.println("없음");
             }
+            System.out.println();
             System.out.println("<혜택 내역>");
             if (receipt.getdDayDiscount() > 0) {
                 System.out.println(String.format("크리스마스 디데이 할인 -%,d", receipt.getdDayDiscount()));
@@ -28,34 +30,42 @@ public class OutputView {
             }
             if (receipt.getDayDiscount() > 0) {
                 if (receipt.isWeekend()) {
-                    System.out.println(String.format("주말 할인 -%,d", receipt.getdDayDiscount()));
+                    System.out.println(String.format("주말 할인 -%,d", receipt.getDayDiscount()));
                 }
                 if (!receipt.isWeekend()) {
-                    System.out.println(String.format("평일 할인 -%,d", receipt.getdDayDiscount()));
+                    System.out.println(String.format("평일 할인 -%,d", receipt.getDayDiscount()));
                 }
             }
             if (receipt.isCanGetEvent()) {
                 System.out.println("증정 이벤트: -25,000원");
             }
+            System.out.println();
             System.out.println("<총혜택 금액>");
             System.out.println(String.format("-%,d원", receipt.getWholeDiscount()));
+            System.out.println();
             System.out.println("<할인 후 예상 결제 금액>");
             System.out.println(String.format("%,d", receipt.getAfterDiscount()));
+            System.out.println();
             System.out.println("<12월 이벤트 배지>");
             System.out.println(receipt.getBadge());
         }
         if (!receipt.isCanEvent()) {
             System.out.println(String.format("%,d원", receipt.getTotalAmount()));
+            System.out.println();
             System.out.println("<증정 메뉴>");
             System.out.println("없음");
+            System.out.println();
             System.out.println("<혜택 내역>");
             System.out.println("없음");
+            System.out.println();
             System.out.println("<총혜택 금액>");
             System.out.println("0원");
+            System.out.println();
             System.out.println("<할인 후 예상 결제 금액>");
             System.out.println(String.format("%,d원", receipt.getTotalAmount()));
+            System.out.println();
             System.out.println("<12월 이벤트 배지>");
-            System.out.println("없음");
+            System.out.println(receipt.getBadge());
         }
     }
 }
