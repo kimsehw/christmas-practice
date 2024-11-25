ada1c3febe5724a68827e76d66fc2e21550ef251
kimsehw <seunghwan9703@gmail.com>
2024-11-25 20:23:20 +0900

    feat(Controller): 재입력 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,11 +1,14 @@
 package christmas;
 
 import christmas.controller.Controller;
+import christmas.model.Planner;
+import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Controller controller = new Controller();
+        Controller controller = new Controller(new InputView(null), new OutputView(), new Planner());
         controller.run();
     }
 }
--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,5 +1,6 @@
 package christmas.controller;
 
+import christmas.exception.InputException;
 import christmas.model.Date;
 import christmas.model.InputDateParser;
 import christmas.model.Planner;
@@ -8,17 +9,45 @@ import christmas.model.order.Orders;
 import christmas.view.InputView;
 import christmas.view.InputViewFactory;
 import christmas.view.OutputView;
+import java.util.function.Supplier;
 
 public class Controller {
 
+    InputView inputView;
+    OutputView outputView;
+    Planner planner;
+
+    public Controller(InputView inputView, OutputView outputView, Planner planner) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.planner = planner;
+    }
+
     public void run() {
-        InputView inputView = InputViewFactory.createDateInputVIew();
-        InputDateParser inputDateParser = new InputDateParser();
-        Date date = inputDateParser.parse(inputView.getInput());
-        inputView = InputViewFactory.createMenuInputVIew();
-        Orders orders = new InputOrderParser().parseOrders(inputView.getInput());
-        Planner planner = new Planner();
-        OutputView outputView = new OutputView();
+        Date date = handle(this::handleDateInput);
+        Orders orders = handle(this::handleOrdersInput);
         outputView.display(planner.calculateAmount(date, orders));
+    }
+
+    private <T> T handle(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (InputException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private Date handleDateInput() {
+        inputView = InputViewFactory.createDateInputVIew();
+        InputDateParser inputDateParser = new InputDateParser();
+        return inputDateParser.parse(inputView.getInput());
+    }
+
+    private Orders handleOrdersInput() {
+        inputView = InputViewFactory.createMenuInputVIew();
+        InputOrderParser inputOrderParser = new InputOrderParser();
+        return inputOrderParser.parseOrders(inputView.getInput());
     }
 }
